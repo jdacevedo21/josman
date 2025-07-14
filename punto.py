a819ac1800c1121b813c00c195081ef6f1f9042f
jdacevedo21 <josmanacevedo9@gmail.com>
2025-07-14 15:15:40 -0500

    Update punto.py

---
+++ punto.py
@@ -0,0 +1,12 @@
+v1=int(input("Ingrese el valor del v1: "))
+v2=int(input("Ingrese el valor del v2: "))
+v3=int(input("Ingrese el valor del v3: "))
+v4=int(input("Ingrese el valor del v4: "))
+v5=int(input("Ingrese el valor del v5: "))
+voltaje_total= v1+v2+v3+v4+v5
+promedio= voltaje_total /5
+print("el promedio del voltaje es:", promedio)
+if promedio >220:
+ print("el promedio es mayor a 220")
+else:
+ print("el promedio es menor a 220")
